a949154b3983ced27460ff9cbe72b1d123c7b6ad
Hardik Modha <hardik.modha27@gmail.com>
2024-11-28 20:50:39 +0530

    Initial Commit

---
+++ 7SpanPractical/_SpanPracticalApp.swift
@@ -0,0 +1,17 @@
+//
+//  _SpanPracticalApp.swift
+//  7SpanPractical
+//
+//  Created by Hardik Modha on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct _SpanPracticalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
